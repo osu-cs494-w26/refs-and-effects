0835fa211bb8d4d78789f6553b40b49f1fb89717
Rob Hess <hessro@oregonstate.edu>
2026-01-15 15:20:28 -0800

    Add code from 1/15 lecture.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import VideoPlayer from './VideoPlayer'
 
@@ -7,35 +7,61 @@ import kittiesVideo from './assets/kitties.mp4'
 import './App.css'
 
 export default function PhotosApp() {
+    const counter = useRef(0)
+    console.log("== counter:", counter)
     const [ videoIsPlaying, setVideoIsPlaying ] = useState(false)
+
+    const firstPhotoRef = useRef(null)
+    const secondPhotoRef = useRef(null)
+    const thirdPhotoRef = useRef(null)
+    const videoRef = useRef(null)
+
     return (
         <>
             <nav>
-                <button>
+                <button onClick={() => {
+                    counter.current++
+                    console.log("== counter.current:", counter.current)
+                }}>
+                    Counter: {counter.current}
+                </button>
+                <button onClick={() => firstPhotoRef.current.scrollIntoView({
+                    block: "center",
+                    inline: "center"
+                })}>
                     Go to first photo
                 </button>
-                <button>
+                <button onClick={() => secondPhotoRef.current.scrollIntoView({
+                    block: "center",
+                    inline: "center"
+                })}>
                     Go to second photo
                 </button>
-                <button>
+                <button onClick={() => thirdPhotoRef.current.scrollIntoView({
+                    block: "center",
+                    inline: "center"
+                })}>
                     Go to third photo
                 </button>
-                <button>
+                <button onClick={() => videoRef.current.scrollIntoView({
+                    block: "center",
+                    inline: "center"
+                })}>
                     Go to cat video
                 </button>
             </nav>
             <main>
-                <div>
+                <div ref={firstPhotoRef}>
                     <img src="https://picsum.photos/id/100/480" />
                 </div>
-                <div>
+                <div ref={secondPhotoRef}>
                     <img src="https://picsum.photos/id/200/480" />
                 </div>
-                <div>
+                <div ref={thirdPhotoRef}>
                     <img src="https://picsum.photos/id/300/480" />
                 </div>
-                <div>
-                    <VideoPlayer src={kittiesVideo} />
+                <div ref={videoRef}>
+                    <VideoPlayer src={kittiesVideo} isPlaying={videoIsPlaying} />
                     <div>
                         <button onClick={() => setVideoIsPlaying(prev => !prev)}>
                             {videoIsPlaying ? "⏸️" : "▶️"}
--- src/VideoPlayer.jsx
+++ src/VideoPlayer.jsx
@@ -1,4 +1,16 @@
+import { useRef, useEffect } from 'react'
+
 export default function VideoPlayer(props) {
     const { src, isPlaying } = props
-    return <video src={src} loop playsInline />
+    const ref = useRef(null)
+
+    useEffect(() => {
+        if (isPlaying) {
+            ref.current.play()
+        } else {
+            ref.current.pause()
+        }
+    })
+
+    return <video ref={ref} src={src} loop playsInline />
 }
